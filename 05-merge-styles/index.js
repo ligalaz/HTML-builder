e50e5efdd1f88775749d53050ef247e6d196041f
ligalaz <95artm.mick@gmail.com>
2022-05-21 22:14:07 +0300

    feat:add 05-merge-styles solution (Sat, May 21, 10:14:10 PM)

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const fs = require('fs');
+const { readdir } = require('fs/promises');
+
+const pathCurrentDir = path.join(__dirname, 'styles');
+const writeStream = fs.createWriteStream(
+  path.join(__dirname, 'project-dist', 'bundle.css')
+);
+
+(async function bundler() {
+  try {
+    let files = await readdir(pathCurrentDir, { withFileTypes: true });
+    for (let file of files) {
+      if (file.isFile() && /\.css$/.test(file.name)) {
+        fs.createReadStream(path.join(pathCurrentDir, file.name)).pipe(
+          writeStream
+        );
+      } else null;
+    }
+  } catch (err) {
+    console.log('Ошибка сборки', err.message);
+  }
+})();
