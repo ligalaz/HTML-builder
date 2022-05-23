a262f76419f5921b6d87f42aaa7b610bfb5ca1bd
ligalaz <95artm.mick@gmail.com>
2022-05-23 18:27:12 +0300

    fix:delete recursive folder check 03 task (Mon, May 23, 06:27:10 PM)

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -7,17 +7,16 @@ const { readdir } = require('fs/promises');
     const files = await readdir(pathName, { withFileTypes: true });
     for (let file of files)
       if (!file.isFile()) {
-        deapRead(path.join(pathName, `${file.name}`));
+        null;
       } else {
-        if (!/^\.git/.test(file.name))
-          fs.stat(path.join(pathName, `${file.name}`), (err, stats) => {
-            if (err) throw err;
-            console.log(
-              `${path.parse(file.name).name} - ${path
-                .parse(file.name)
-                .ext.replace(/^\./g, '')} - ${(stats.size / 1024).toFixed(1)}kb`
-            );
-          });
+        fs.stat(path.join(pathName, `${file.name}`), (err, stats) => {
+          if (err) throw err;
+          console.log(
+            `${path.parse(file.name).name} - ${path
+              .parse(file.name)
+              .ext.replace(/^\./g, '')} - ${(stats.size / 1024).toFixed(1)}kb`
+          );
+        });
       }
   } catch (err) {
     console.error('Директория пуста', err.message);
